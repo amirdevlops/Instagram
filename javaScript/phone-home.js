84251041cbfb2a3a7d94ecddfde802d4ab515113
amirdevlops <137190254+amirdevlops@users.noreply.github.com>
2023-08-28 19:46:43 +0530

    Add files via upload

--- javaScript/phone-home.js
+++ javaScript/phone-home.js
@@ -1,3 +1,64 @@
+const slider = document.getElementById("slider");
+let slideIndex = 0;
+let startX = 0;
+let currentX = 0;
+
+slider.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+  currentX = startX;
+}, { passive: true });
+
+slider.addEventListener("touchmove", (e) => {
+  if (startX === null) {
+    return;
+  }
+
+  currentX = e.touches[0].clientX;
+  const deltaX = currentX - startX;
+
+  // Check if it's the last slide and prevent further scroll if needed
+  if ((slideIndex === slider.children.length - 1 && deltaX < 0) ||
+      (slideIndex === 0 && deltaX > 0)) {
+    return;
+  }
+
+  slider.style.transform = `translateX(calc(-${slideIndex * 100}% + ${deltaX}px))`;
+
+  e.preventDefault();
+}, { passive: false });
+
+slider.addEventListener("touchend", () => {
+  const deltaX = currentX - startX;
+  if (deltaX > 50) {
+    showSlide(slideIndex - 1);
+  } else if (deltaX < -50) {
+    showSlide(slideIndex + 1);
+  } else {
+    showSlide(slideIndex);
+  }
+
+  startX = null;
+});
+
+function showSlide(index) {
+  if (index < 0) {
+    index = 0;
+  } else if (index >= slider.children.length) {
+    index = slider.children.length - 1;
+  }
+  slider.style.transform = `translateX(-${index * 100}%)`;
+  slideIndex = index;
+}
+
+showSlide(slideIndex);
+
+
+
+
+
+
+/*functions to change svg files to show emotions */
+
 const changeSvg = () => {
     let ReelsSvgCont = document.getElementById("reels-horizontal-header");
     let homesSvgCont = document.getElementById("home-horizontal-header");
@@ -46,3 +107,43 @@ let heartSvg = document.getElementById("phone-heart-svg-cont")
 heartSvg.addEventListener("click", ()=>{
 heartSvg.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" id="phone-svg-horizonatal-header"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>`
 });
+
+//this thing will be change accordingly to the reposnse data 
+
+let commentsectionsavecont = document.getElementById("comment-section-save-cont");
+commentsectionsavecont.addEventListener("click", ()=>{
+const commentSectionSave = document.getElementById("comment-section-save");
+if(commentSectionSave.classList == "fillwhite"){
+  commentSectionSave.classList.remove("fillwhite");
+}
+else{
+
+  commentSectionSave.classList.add("fillwhite");
+}
+
+});
+
+let commentsectionheartid = document.getElementById("comment-section-heart-id");
+
+commentsectionheartid.addEventListener("click", ()=>{
+  const redHeart = `<svg aria-label="Unlike" class="x1lliihq x1n2onr6" color="rgb(255, 48, 64)" fill="rgb(255, 48, 64)" height="24" role="img" viewBox="0 0 48 48" width="24"><title>Unlike</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
+  `
+  const unliked = ` <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
+  id="comment-section-heart-svg"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+  <path
+      d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+</svg>`
+  if(commentsectionheartid.innerHTML == redHeart){
+    commentsectionheartid.innerHTML = unliked
+  }
+  else{
+    commentsectionheartid.innerHTML= redHeart;
+  }
+
+});
+
+
+let aoudiomakeroriganalcont = document.querySelector(".aoudio-maker-origanal-cont");
+
+aoudiomakeroriganalcont.innerHTML = `<b class=" aoudio-maker-origanal">sponcerd</b>`
+
